9e068f491ed2d51574ccb8482acdfb65779db4e4
Sean053047 <sadlamb910803@gmail.com>
2022-07-03 03:04:35 +0800

    copy all

--- python/0510.py
+++ python/0510.py
@@ -1,8 +1,13 @@
-def usage():
-    '''
-    this part is the document of usage.
-    '''
-    print('usage has been used.')
+class A:
+    first = 1
+    def __init__(self):
+        self.second = 2
+aa = A()
+bb = A()
 
-help(usage)
-usage()
+bb.first = 11
+print(id(bb.first))
+print(id(A.first))
+A.first = 10
+print(id(bb.first))
+print(id(A.first))
